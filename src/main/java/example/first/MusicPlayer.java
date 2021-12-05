3acf8a0f30502de6a214b33bc620130d9778dc9a
EdemID <ed.idrisov@mail.ru>
2021-12-05 17:48:04 +0300

    Автоматическое внедрение двух и более зависимостей через конструктор

--- src/main/java/example/first/MusicPlayer.java
+++ src/main/java/example/first/MusicPlayer.java
@@ -9,13 +9,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class MusicPlayer {
 
-    @Autowired
     private Classical classical;
+    private Rock rock;
+
+    @Autowired
+    public MusicPlayer(Classical classical, Rock rock) {
+        this.classical = classical;
+        this.rock = rock;
+    }
 
     public MusicPlayer() {
     }
 
     public void playMusic() {
         System.out.println(classical.getSong());
+        System.out.println(rock.getSong());
     }
 }
